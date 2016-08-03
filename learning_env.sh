e63b94a96184460701f7f54801f929e8f83ecfaf
Mark Fernandes <mfernandes61@gmail.com>
2016-08-03 15:55:36 +0100

    Update learning_env.sh

--- learning_env.sh
+++ learning_env.sh
@@ -1,2 +1,4 @@
-/etc/init.d/lighthttpd start
-/scripts/launchsiab.sh
+#!/bin/bash
+echo "Launching SIAB!"
+/etc/init.d/lighthttpd start && /usr/local/bin/shellinaboxd -t -b && tail -f /dev/null
+echo "Running SIAB"
